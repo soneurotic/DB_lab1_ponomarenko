4b297f58d12b3fead232e39a1fc2c189470087c7
Alex <90375900+soneurotic@users.noreply.github.com>
2024-03-27 16:17:30 +0200

    upd: additional task

--- lab1.py
+++ lab1.py
@@ -1,6 +1,7 @@
 from time import perf_counter
 from threading import Thread
 import psycopg2
+import random
 
 username = 'postgres'
 password = 'postgres'
@@ -8,82 +9,146 @@ database = 'DB_sem6_lab1'
 host = 'localhost'
 port = '5432'
 
-# під'єднання до існуючої БД
-conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
-print(type(conn))
+additional_task_query = '''
+DROP TABLE IF EXISTS random_user_counter;
 
-functions = []
+CREATE TABLE random_user_counter (
+    user_id SERIAL PRIMARY KEY,
+    counter INT,
+    version INT
+);
 
-def lost_update():
-    inner_conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
-    inner_cursor = inner_conn.cursor()
-    for _ in range(10_000):
-        inner_cursor.execute(f"SELECT counter FROM user_counter WHERE user_id = 1")
-        counter = inner_cursor.fetchone()[0]
-        counter += 1
-        inner_cursor.execute(f"UPDATE user_counter SET counter = {counter} WHERE user_id = 1")
-        inner_conn.commit()
-functions.append(lost_update)
+INSERT INTO random_user_counter (user_id, counter, version)
+SELECT generate_series(1, 100000), 0, 0;
 
-cursor = conn.cursor()
-def in_place_update():
-    for _ in range(10_000):
-        cursor.execute(f"UPDATE user_counter SET counter = counter + 1 WHERE user_id = 1")
-        conn.commit()
-functions.append(in_place_update)
+SELECT * FROM random_user_counter;
+'''
 
-def row_level_locking():
-    inner_conn = psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port)
-    inner_cursor = inner_conn.cursor()
-    for _ in range(10_000):
-        inner_cursor.execute(f"SELECT counter FROM user_counter WHERE user_id = 1 FOR UPDATE")
-        counter = inner_cursor.fetchone()[0]
-        counter += 1
-        inner_cursor.execute(f"UPDATE user_counter SET counter = {counter} WHERE user_id = 1")
-        inner_conn.commit()
-functions.append(row_level_locking)
 
-def optimistic_concurrency_control():
-    inner_cursor = conn.cursor()
-    for _ in range(10_000):
-        while True:
-            inner_cursor.execute(f"SELECT counter, version FROM user_counter WHERE user_id = 1")
-            result = inner_cursor.fetchone()
-            counter, version = result[0], result[1]
-            counter = counter + 1
-            inner_cursor.execute(
-                f"UPDATE user_counter SET counter = {counter}, version = {version + 1} WHERE user_id = 1 AND version = {version}")
-            conn.commit()
-            count = inner_cursor.rowcount
-            if count > 0:
-                break
-functions.append(optimistic_concurrency_control)
+def main_task(table_name):
+    functions = []
 
-for func in functions:
-    start_time = perf_counter()
+    # additional info (моє бачення):
+    # коли транзакція читає об'єкт користуючись cursor'ом, об'єкт блокується на зміну інших
+    # транзакцій, поки об'єкт не буде відпущено, або поки не відбудеться коміт, тому для
+    # усіх методів окрім in-place update бажане створення курсору всередині них для
+    # подальшого використання функцій у потоках з метою уникнення непередбачуваної поведінки
 
-    # створюємо і запускаємо 10 потоків
-    threads = []
-    for _ in range(10):
-        t = Thread(target=func)
-        threads.append(t)
-        t.start()
+    with psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port) as outer_conn:
+        if table_name == 'random_user_counter':
+            with outer_conn.cursor() as cursor_1:
+                cursor_1.execute(additional_task_query)
+                outer_conn.commit()
 
-    # чекаємо, поки виконаються потоки
-    for t in threads:
-        t.join()
+        # кожен з потоків виконує роботу (селектять у запиті каунтер для апдейту на +1),
+        # швидше ніж встигає відбутися комміт змін попередніх потоків. Але все ж таки між
+        # ця "мікрорізниця" у часі накоплюється у життєвому циклі програми, тож на виході воно
+        # виливається у ~800 "зайвих" значень каунтера поверх тих 10-и тисяч
+        def lost_update():
+            with psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port) as conn:
+                with conn.cursor() as cursor:
+                    for _ in range(10_000):
+                        id_val = get_row() if table_name == 'random_user_counter' else 1
 
-    end_time = perf_counter()
+                        cursor.execute(f"SELECT counter FROM {table_name} WHERE user_id = {id_val}")
+                        counter = cursor.fetchone()[0]
+                        counter += 1
+                        cursor.execute(f"UPDATE {table_name} SET counter = {counter} WHERE user_id = {id_val}")
+                        conn.commit()
 
-    print(f'\n\nВиконана функція: {str(func).split()[1]}')
+        functions.append(lost_update)
 
-    print(f'Виконання зайняло {end_time - start_time: 0.2f} секунд.')
+        # апдейт відбувається безпосередньо у БД, СУБД якої по дефолту передбачає read commited для транзакцій
+        def in_place_update():
+            with psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port) as conn:
+                with conn.cursor() as cursor:
+                    for _ in range(10_000):
+                        id_val = get_row() if table_name == 'random_user_counter' else 1
+                        cursor.execute(f"UPDATE {table_name} SET counter = counter + 1 WHERE user_id = {id_val}")
+                        conn.commit()
 
-    cursor.execute(f"SELECT counter, version FROM user_counter WHERE user_id = 1")
-    result = cursor.fetchone()
-    counter, version = result[0], result[1]
+        functions.append(in_place_update)
 
-    print(f'Остаточне значення у таблиці: counter = {counter}, version = {version}')
+        # кожен вибір каунтера, що здійснюється з програми до БД, активує локінг рядка бази даних для апдейту
+        def row_level_locking():
+            with psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port) as conn:
+                with conn.cursor() as cursor:
+                    for _ in range(10_000):
+                        id_val = get_row() if table_name == 'random_user_counter' else 1
+                        cursor.execute(f"SELECT counter FROM {table_name} WHERE user_id = {id_val} FOR UPDATE")
+                        counter = cursor.fetchone()[0]
+                        counter += 1
+                        cursor.execute(f"UPDATE {table_name} SET counter = {counter} WHERE user_id = {id_val}")
+                        conn.commit()
 
-    cursor.execute(f'UPDATE user_counter SET counter = 0, version = 0')
-    conn.commit()
+        functions.append(row_level_locking)
+
+        # optimistic concurrency control використовує додатковим параметром версію
+        # для того, щоб трекати чи не пройшов паралельно апдейт значення, що змінюється
+        def optimistic_concurrency_control():
+            with psycopg2.connect(user=username, password=password, dbname=database, host=host, port=port) as conn:
+                with conn.cursor() as cursor:
+                    for _ in range(10_000):
+                        id_val = get_row() if table_name == 'random_user_counter' else 1
+
+                        # якщо наш апдейт не пройде, то нам треба виконати ретрай, бо нам треба ітерувати
+                        # допоки він стовідсотково не пройде. Для цього ми використовуємо нескінченний цикл
+                        # що працюватиме до моменту коли не пройде остання (10-тисячна) транзакція потоку
+                        while True:
+                            cursor.execute(f"SELECT counter, version FROM {table_name} WHERE user_id = {id_val}")
+                            result = cursor.fetchone()
+                            counter, version = result[0], result[1]
+                            counter = counter + 1
+                            cursor.execute(
+                                f"UPDATE {table_name} SET counter = {counter}, version = {version + 1} WHERE user_id = {id_val} AND version = {version}")
+                            conn.commit()
+                            count = cursor.rowcount
+                            if count > 0:
+                                break
+
+        functions.append(optimistic_concurrency_control)
+
+        for func in functions:
+            with outer_conn.cursor() as cursor:
+                start_time = perf_counter()
+
+                # створюємо і запускаємо 10 потоків
+                threads = []
+                for _ in range(10):
+                    t = Thread(target=func)
+                    threads.append(t)
+                    t.start()
+
+                # чекаємо, поки виконаються потоки
+                for t in threads:
+                    t.join()
+
+                end_time = perf_counter()
+
+                print(f'\n\nВиконана функція: {str(func).split()[1]}')
+
+                print(f'Виконання зайняло {end_time - start_time: 0.2f} секунд.')
+
+                if table_name == 'user_counter':
+                    cursor.execute(f"SELECT counter, version FROM {table_name} WHERE user_id = 1")
+                    result = cursor.fetchone()
+                    counter, version = result[0], result[1]
+
+                    print(f'Остаточне значення у таблиці: counter = {counter}, version = {version}')
+
+                    cursor.execute(f'UPDATE {table_name} SET counter = 0, version = 0')
+
+                outer_conn.commit()
+
+
+def get_row():
+    choice = random.randint(0, 1)
+    if choice == 0:
+        return 1
+    else:
+        return random.randint(2, 100_000)
+
+
+main_task(table_name='user_counter')
+
+# main_task(table_name='random_user_counter')
